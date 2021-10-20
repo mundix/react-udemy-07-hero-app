6be3c65e5bfac74fcd1a0dfa8419e8d561c8227e
Edmundo Pichardo <ce.pichardo@gmail.com>
2021-10-20 04:25:43 -0400

    Settled the PriveRoute Component with props and properties

--- src/routers/AppRouter.js
+++ src/routers/AppRouter.js
@@ -1,22 +1,31 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import {
     BrowserRouter as Router,
     Switch,
     Route,
 } from "react-router-dom";
 
+import { PrivateRoute } from './PrivateRoute';
+
 import { LoginScreen } from '../components/Login/LoginScreen';
 import { DashboardRoutes } from './DashboardRoutes';
+import { AuthContext } from '../components/auth/AuthContext';
 
 
 export const AppRouter = () => {
+
+    const {user} = useContext(AuthContext);
+
     return (
         <Router>
             <div >
-                {/* <Navbar /> */}
                 <Switch>
                     <Route exact path='/login' component={LoginScreen} />
-                    <Route  path='/' component={DashboardRoutes} />
+                    <PrivateRoute  
+                        path='/' 
+                        component={DashboardRoutes} 
+                        isAuthenticated={user.logged}
+                        />
                 </Switch>
             </div>
         </Router>
--- src/routers/PrivateRoute.js
+++ src/routers/PrivateRoute.js
@@ -1,10 +1,26 @@
 import React from 'react';
+import { Route, Redirect } from 'react-router-dom';
+import PropTypes from 'prop-types';
+
 
 // otro functional component , con un compoertamiento especial 
-export const PrivateRoute = () => {
+export const PrivateRoute = ({
+    isAuthenticated,
+    component: Component,
+    ...rest //El resto de los elementos van a caer por aca. 
+}) => {
     return (
-        <div>
-            
-        </div>
+        <Route {...rest}
+            component={ (props) => (
+                (isAuthenticated)
+                ? (<Component {...props} />)
+                : (<Redirect to='/login'/>)
+            )}
+        />
     )
+}
+
+PrivateRoute.propTypes = {
+    isAuthenticated: PropTypes.bool.isRequired,
+    component: PropTypes.func.isRequired
 }
