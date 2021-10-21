2440d81f7fdc90f1dfe42da6f3edb626286666e2
Edmundo Pichardo <ce.pichardo@gmail.com>
2021-10-21 04:19:19 -0400

    Testing on DashboardRoutes take a Snapshot'

---
+++ src/tests/routers/DashboardRoutes.test.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import { shallow, mount } from 'enzyme';
+import { DashboardRoutes } from '../../routers/DashboardRoutes';
+import { AuthContext } from '../../components/auth/AuthContext';
+import { MemoryRouter } from 'react-router-dom';
+
+
+describe('Pruebas en <DashboardRoutes/>', () => {
+    const contextValue = {
+        dispatch: jest.fn(),
+        user: {
+            name: 'Edmundo',
+            logged: true
+        }
+    }
+    test('Debe mostrar correctamente <DashboardRoutes/>', () => {
+        
+        const wrapper = mount(
+            <AuthContext.Provider value={ contextValue }>
+                <MemoryRouter>
+                    <DashboardRoutes/>
+                </MemoryRouter>
+            </AuthContext.Provider>
+            );
+        
+            expect(wrapper).toMatchSnapshot();
+    });
+
+});
