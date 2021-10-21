da1e0f53b164459367472ee4ceb8704952c9a0b0
Edmundo Pichardo <ce.pichardo@gmail.com>
2021-10-21 03:55:40 -0400

    Testing PrivateRoute when is logged or authenticated

---
+++ src/tests/routers/PrivateRoute.test.js
@@ -0,0 +1,37 @@
+import React from 'react';
+import { shallow, mount } from 'enzyme';
+import { PrivateRoute } from '../../routers/PrivateRoute';
+import { MemoryRouter } from 'react-router';
+
+
+describe('Pruebas en <PrivateRoute/>', () => {
+
+    const props = {
+        location: {
+            pathname: '/marvel'
+        }
+    }
+
+    test('Debe de mostrar el compoentne si esta autenticado y guardar locallStorage', () => {
+        //En component se debe mandar un componete o elemento dentro de una func () => ( <element> ... </element>)
+        // Shalow solo renderiza el high order component , el shallow no va a funciinar cuando teng oeste tipo de estructura, 
+        // debo usar el mount dentro de enzyme
+        // const wrapper = shallow(
+            // Si esta auth true , pasa la prueba 
+            // El redirect es un string vacio, por eso cuando esta false, no muestra nada. 
+            // Os ea si esta privado y no esta authenticado 
+        const wrapper = mount(
+            <MemoryRouter>
+                <PrivateRoute
+                    isAuthenticated={true}
+                    component={() => <span>Listo!</span>}
+                    {...props}
+                />
+            </MemoryRouter>
+        );
+        // Da un error por qeu debe estar dentro de un Router, y se usa el MemoryRouter un high order component que contenta el PrivateRoute
+        // console.log(wrapper.html());
+        expect(wrapper.find('span').exists()).toBe(true);
+    });
+
+});
